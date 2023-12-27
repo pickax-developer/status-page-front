da6839edd012a595c98bdf9468458e80e071257c
tjdls111 <tjdls111@naver.com>
2023-12-27 22:48:38 +0900

    setting: 파일 저장 시 자동 format되게 하기

--- src/App.js
+++ src/App.js
@@ -5,13 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1 className="text-3xl font-bold ">
-         Hello world!
-       </h1>
-        
+        <h1 className="text-3xl font-bold">Hello world!</h1>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
