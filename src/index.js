3d220dd9555f04d9ab5356319102314fa314a533
tjdls111 <tjdls111@naver.com>
2024-01-01 12:20:02 +0900

    setting:daisyUI 설치, 불필요한 파일 제거

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+<<<<<<< HEAD
 import { RouterProvider } from 'react-router-dom'
 
 
@@ -12,6 +13,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
+=======
+import App from './App'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+>>>>>>> 8f4eb11 (setting:daisyUI 설치, 불필요한 파일 제거)
   </React.StrictMode>,
 )
 
