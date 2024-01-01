6d68d7f3cd7df3e340a07f3c36bd15729d8fa728
Seoin <68271159+tjdls111@users.noreply.github.com>
2024-01-01 21:38:07 +0900

    SiteList element 추가 (#2)

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -4,12 +4,17 @@ import Layout from '../layouts/index.tsx'
 import SiteDetail from '../pages/SiteDetail.tsx'
 import Login from '../pages/Login.tsx'
 import ErrorPage from '../pages/Error.tsx'
+import SiteList from '../pages/SiteList.tsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
     children: [
+      {
+        path: '/',
+        element: <SiteList />,
+      },
       {
         path: '/:siteId',
         element: <SiteDetail />,
