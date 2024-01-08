8aa8d3f1fa0022c3b85eec8d772213a3844f80ba
tjdls111 <tjdls111@naver.com>
2024-01-08 20:41:12 +0900

    feat: component list

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import SiteRegisterButton from './SiteRegisterButton.tsx'
+import { useParams } from 'react-router-dom'
 
 const Header = () => {
+  const { siteId } = useParams<{ siteId: string }>()
+  const isSiteListPage = !siteId
+
   return (
     <header className="flex justify-between navbar bg-secondary">
       <button className="text-xl btn btn-ghost">Quack Run</button>
-      <SiteRegisterButton />
+      {isSiteListPage && <SiteRegisterButton />}
     </header>
   )
 }
--- src/pages/SiteDetail.tsx
+++ src/pages/SiteDetail.tsx
@@ -1,5 +1,41 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import useComponentList from '../useCases/componentList.ts'
 
 export default function SiteDetail() {
-  return <p>site detail</p>
+  const { siteId } = useParams<{ siteId: string }>()
+  const { data, error, isLoading } = useComponentList({ siteId })
+
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+
+  return (
+    <>
+      <div className="overflow-x-auto min-h-full rounded-md">
+        <table className="table">
+          <thead className="bg-primary text-black">
+            <tr>
+              <th></th>
+              <th>이름</th>
+              <th>설명</th>
+              <th>상태</th>
+            </tr>
+          </thead>
+          <tbody className="rounded-md">
+            {data.length === 0 && <tr> 컴포넌트가 아직 없어요 </tr>}
+            {data.map((component) => (
+              <tr className="bg-base-200" key={component.id}>
+                <td>{component.id}</td>
+                <td>{component.name}</td>
+                <td>{component.description}</td>
+                <td>
+                  <div className="badge badge-primary badge-outline">{component.status}</div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
 }
