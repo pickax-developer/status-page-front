e1b91d7a032a53cbe149fb7be6f3ca6b5140f6d7
tjdls111 <tjdls111@naver.com>
2024-01-16 21:19:18 +0900

    fix: 불확실한 네이밍 변경

--- src/components/useSiteRegisterDialog.ts
+++ src/components/useSiteRegisterDialog.ts
@@ -5,89 +5,88 @@ import { isValidURL } from '../utils/validation.ts'
 import siteMetaTag from '../useCases/siteMetaTag.ts'
  import { toast } from 'react-toastify'
 
- const useSiteRegisterDialog = ({ id }: { id?: number }) => {
-   const [name, setName] = useState<string>('')
-   const [url, setUrl] = useState<string>('')
-   const [description, setDescription] = useState<string>('')
-   const [resId, setResId] = useState<number>(0)
-   const [metaTag, setMetaTag] = useState<string>('')
-   const [step, setStep] = useState<number>(1)
-   const [isDisabledNextBtn, setIsDisabledNextBtn] = useState<boolean>(true)
+const useSiteRegisterDialog = ({ id }: { id?: number }) => {
+  const [name, setName] = useState<string>('')
+  const [url, setUrl] = useState<string>('')
+  const [description, setDescription] = useState<string>('')
+  const [siteId, setSiteId] = useState<number>(0)
+  const [metaTag, setMetaTag] = useState<string>('')
+  const [step, setStep] = useState<number>(1)
+  const [isDisabledNextBtn, setIsDisabledNextBtn] = useState<boolean>(true)
 
-   const onClickNextButton = async () => {
-     try {
-       const res = await postSite({ name, url, description })
-       setMetaTag(res.metaTag)
-       setResId(res.id)
-       setStep(2)
-     } catch {
-       toast('사이트 등록에 실패했습니다. 다시 시도해주세요.')
-     }
-   }
+  const onClickNextButton = async () => {
+    try {
+      const res = await postSite({ name, url, description })
+      setMetaTag(res.metaTag)
+      setSiteId(res.id)
+      setStep(2)
+    } catch {
+      toast('사이트 등록에 실패했습니다. 다시 시도해주세요.')
+    }
+  }
 
-   const onClickCheckButton = async () => {
-     try {
-       await siteCheck({ id: resId })
-       setStep(3)
-     } catch {
-       setStep(4)
-     }
-   }
+  const onClickCheckButton = async () => {
+    try {
+      await siteCheck({ id: siteId })
+      setStep(3)
+    } catch {
+      setStep(4)
+    }
+  }
 
-   const copyMetaTag = () => {
-     navigator.clipboard.writeText(`<meta name="quack-run-service-verification" content="${metaTag}">`)
-   }
+  const copyMetaTag = () => {
+    navigator.clipboard.writeText(`<meta name="quack-run-service-verification" content="${metaTag}">`)
+  }
 
-   const onCloseModal = () => {
-     setDescription('')
-     setMetaTag('')
-     setName('')
-     setUrl('')
-     setStep(1)
-     ;(document.getElementById('site_register_dialog') as HTMLDialogElement)?.close()
-   }
+  const onCloseModal = () => {
+    setDescription('')
+    setMetaTag('')
+    setName('')
+    setUrl('')
+    setStep(1)
+    ;(document.getElementById('site_register_dialog') as HTMLDialogElement)?.close()
+  }
 
-   useEffect(() => {
-     setIsDisabledNextBtn(name.length === 0 || !isValidURL(url))
-   }, [name, url])
+  useEffect(() => {
+    setIsDisabledNextBtn(name.length === 0 || !isValidURL(url))
+  }, [name, url])
 
-   useEffect(() => {
-     const fetchData = async () => {
-       if (id) {
-         try {
-           const res = await siteMetaTag({ id })
-           setMetaTag(res.content)
-           setStep(2)
-         } catch {
-           ;(document.getElementById('site_register_dialog') as HTMLDialogElement)?.close()
-           toast('사이트 메타 태그 정보 불러오기에 실패했습니다. 다시 시도해주세요.')
-           //TODO: 새 메타 태그 정보 발급받기 api 호출
-         }
-       }
-     }
+  useEffect(() => {
+    const fetchData = async () => {
+      if (id) {
+        try {
+          const res = await siteMetaTag({ id })
+          setMetaTag(res.content)
+          setStep(2)
+        } catch {
+          ;(document.getElementById('site_register_dialog') as HTMLDialogElement)?.close()
+          toast('사이트 메타 태그 정보 불러오기에 실패했습니다. 잠시 후 다시 시도해주세요.')
+        }
+      }
+    }
 
-     if (id) {
-       setResId(id)
-       fetchData()
-     }
-   }, [id])
+    if (id) {
+      setSiteId(id)
+      fetchData()
+    }
+  }, [id])
 
-   return {
-     name,
-     url,
-     description,
-     setName,
-     setUrl,
-     setDescription,
-     onClickNextButton,
-     metaTag,
-     onClickCheckButton,
-     copyMetaTag,
-     step,
-     setStep,
-     onCloseModal,
-     isDisabledNextBtn,
-   }
- }
+  return {
+    name,
+    url,
+    description,
+    setName,
+    setUrl,
+    setDescription,
+    onClickNextButton,
+    metaTag,
+    onClickCheckButton,
+    copyMetaTag,
+    step,
+    setStep,
+    onCloseModal,
+    isDisabledNextBtn,
+  }
+}
 
 export default useSiteRegisterDialog
