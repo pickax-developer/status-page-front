b2cda62b54e9c4591c7c5644ab8823beb9b3b4ba
tjdls111 <tjdls111@naver.com>
2024-02-01 21:19:41 +0900

    style: 불필요한 코드 제거, 사이드 바 스타일

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,7 +9,7 @@ const Sidebar = () => {
   }
   return (
     <>
-      <div className="w-56 menu bg-primary text-black">
+      <div className="w-56 menu bg-primary text-black justify-between">
         <ul>
           <li>
             <Link to="/">
@@ -30,46 +30,8 @@ const Sidebar = () => {
               Site List
             </Link>
           </li>
-          <li>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              Item 1
-            </a>
-          </li>
-          <li>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                />
-              </svg>
-              Item 3
-            </a>
-          </li>
         </ul>
-        <div className='flex flex-col gap-5 mt-10'>
+        <div className="flex flex-col gap-5 mb-20">
           <button className="btn" type="button" onClick={() => onClickLogout()}>
             로그아웃
           </button>
