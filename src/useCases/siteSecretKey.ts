6b50d19c7ab27cf9495f095ae74eb33a9af8f0e3
tjdls111 <tjdls111@naver.com>
2024-02-01 19:07:41 +0900

    fix: secret key GET api의 siteId를 undefined로 호출하는 에러

--- src/useCases/siteSecretKey.ts
+++ src/useCases/siteSecretKey.ts
@@ -2,7 +2,8 @@ import useSWR from 'swr'
 import { BASE_URL } from '../common/api'
 import { SiteSecretKeyResponse } from '../types/response/site.ts'
 
-const useSiteSecretKey = ({ siteId }: { siteId?: number }) => {
+const useSiteSecretKey = ({ siteId }: { siteId?: number | 'undefined' }) => {
+  if (!siteId || siteId === 'undefined') return { error: 'siteId is required' }
   const fetcher = (...args) => fetch(...args).then((res) => res.json())
   const { data, error, isLoading } = useSWR<SiteSecretKeyResponse, Error>(
     `${BASE_URL}/sites/${siteId}/secret-key`,
