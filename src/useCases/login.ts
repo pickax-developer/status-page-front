4c09ef5f7b33fee1c2b689421b4799a8252114bc
tjdls111 <tjdls111@naver.com>
2024-02-04 22:05:23 +0900

    fix: 로그인 성공 시 페이지 이동

--- src/useCases/login.ts
+++ src/useCases/login.ts
@@ -4,14 +4,16 @@ import { LoginResponse } from '../types/response/auth.ts'
 
 const login = async ({ email, password }: { email: string; password: string }) => {
   try {
-    const response = await axiosInstance.post<LoginResponse>(`/login`, {
+    const response = await axiosInstance.post<LoginResponse>(`/auth/login`, {
       email,
       password,
     })
     const { accessToken, accessTokenExpiresIn } = response.data
-    const item = { value: accessToken, expiry: accessTokenExpiresIn }
+    const item = { value: accessToken, expiredIn: accessTokenExpiresIn }
     localStorage.setItem('accessToken', JSON.stringify(item))
     axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
+    location.replace('/')
+  
     return response.data
   } catch (error) {
     toast('로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요.')
